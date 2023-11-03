271eaa1d3d1172019adac21ba12b6f127dd3f774
igorferrati <igorferrati93@gmail.com>
2023-11-03 11:03:37 -0300

    update repo

--- infra/module-wp/main.tf
+++ infra/module-wp/main.tf
@@ -4,7 +4,7 @@ module "vm_wp" {
   instance_zone01 = "us-central1-a"
   instance_zone02 = "us-central1-b"
   # instance_zone03 = "us-central1-c"
-  gcp_project = "igor-398900"
+  gcp_project = "project-id"
   gcp_region =  "us-central1"
   gcp_zone = "us-central1-a"
   # instance_name = "ubuntu-node"
